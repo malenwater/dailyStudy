cc8f8aa617e2cbdef2c968d5ba559709bf76efdb
malenwater <malenwater@ajou.ac.kr>
2024-09-23 14:33:33 +0900

    백준
    1. 골드 문제 틀린거 풀이

---
+++ 기술/백준/2024_09/11401/11401.cpp
@@ -0,0 +1,43 @@
+// 2024/09/20
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector <long long> v;
+const long long int MOD = 1000000007;
+ long long int mod_reverse(long long int N){
+    long long result = 1;
+    long long exp = MOD - 2; 
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * N) % MOD;
+        }
+        N = (N * N) % MOD;
+        exp /= 2;
+    }
+    return result;
+}
+void factor(long long N){
+    v.resize(N + 1);
+    v[0] = 1;
+    for(long long i=1;i<=N;i++){
+        v[i] = (v[i-1] * i) % MOD;
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long N,K;
+    cin >> N >> K;
+    factor(N);
+    // cout << v.at(N) << endl;
+    // cout << v[N ]<< endl;
+    // cout << v.at(N - K) << endl;
+    // cout << v.at(K) << endl;
+    // cout << (v.at(N) / (v.at(N - K) * v.at(K) % 1000000007) % 1000000007) << "\n";
+    // cout << mod_reverse(v.at(N - K)) << endl;
+    // cout << mod_reverse(v.at(K)) << endl;
+    long long result = (v[N] * mod_reverse(v[K]) % MOD * mod_reverse(v[N - K]) % MOD) % MOD;
+    cout << result << "\n";
+    return 0;
+}
